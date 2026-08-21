31e9c6588c683ab54a206a0c6de9cf755a364d6a
Richard Rodger <richard@ricebridge.com>
2026-08-21 01:14:41 +0100

    release: v0.3.5 (unify go+ts, patch bump)

--- go/go.mod
+++ go/go.mod
@@ -2,6 +2,6 @@ module github.com/tabnas/path/go
 
 go 1.24.7
 
-require github.com/tabnas/parser/go v0.8.10
+require github.com/tabnas/parser/go v0.8.11
 
-require github.com/tabnas/support/go v0.3.2
+require github.com/tabnas/support/go v0.3.3
--- go/path.go
+++ go/path.go
@@ -9,7 +9,7 @@ import (
 // VERSION is this module's version. It MUST equal ts/package.json
 // "version": the release orchestrator rewrites both, and
 // TestVersionMatchesPackageJSON fails the build if they drift.
-const VERSION = "0.3.4"
+const VERSION = "0.3.5"
 
 type PathOptions struct{}
 
